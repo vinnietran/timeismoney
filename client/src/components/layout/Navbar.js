4c35778672f2734ee9da2d535658d917573bc891
VinTran <tranquillo.vinnie@gmail.com>
2019-06-20 21:01:48 -0400

    Added protected nav bar links as well as welcome message

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,38 +1,53 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
 
 const Navbar = ({ title, icon }) => {
-    return (
-        <div className="navbar bg-primary">
-            <h1>
-                <i className={icon} /> { title }
-            </h1>
-            <ul>
-            <li>
-                <Link to='/'>Home</Link>
-            </li>
-            <li>
-                <Link to='/about'>About</Link>
-            </li>
-            <li>
-                <Link to='/register'>Register</Link>
-            </li>
-            <li>
-                <Link to='/Login'>Login</Link>
-            </li>
-            </ul>
-        </div>
-    )
-}
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+
+  const authLinks = (
+    <Fragment>
+      <li>Hello, {user && user.firstName}</li>
+      <li>
+        <a href="#!">
+          <i className="fas fa-sign-out-alt" />{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+       <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/Login">Login</Link>
+        </li>
+    </Fragment>
+  );
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+       {isAuthenticated ? authLinks : guestLinks}
+      </ul>
+    </div>
+  );
+};
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string,
-}
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
 
 Navbar.defaultProps = {
-    title: 'Time Is Money',
-    icon: 'fas fa-id-card-alt'
-
-}
-export default Navbar
+  title: "Time Is Money",
+  icon: "fas fa-id-card-alt"
+};
+export default Navbar;
