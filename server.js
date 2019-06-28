0c7a19d4c192645ec9083468a684db6509b729dc
VinTran <tranquillo.vinnie@gmail.com>
2019-06-28 13:44:52 -0400

    preparing for deploy

--- server.js
+++ server.js
@@ -10,12 +10,20 @@ connectDB();
 app.use(express.json({ extended: false })); 
 
 
-app.get('/', (req, res) => res.json({ msg: "Welcome to TIM"}));
+
 
 //define routes 
 app.use('/api/users', require('./routes/user'))
 app.use('/api/time', require('./routes/time'))
 app.use('/api/auth', require('./routes/auth'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+  }
 
 const PORT = process.env.PORT || 5000
 
