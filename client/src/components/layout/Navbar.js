b0f7f70834067e59aee589995c3477307dd5d3a5
VinTran <tranquillo.vinnie@gmail.com>
2019-06-29 12:39:07 -0400

    heroku issues

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -23,7 +23,6 @@ const Navbar = ({ title, icon }) => {
       <li><Link to="/admin">Generate Invoice</Link></li>
       <li>
         <a onClick={onLogout} href="#!">
-          <i className="fas fa-sign-out-alt" />{" "}
           <span className="hide-sm">Logout</span>
         </a>
       </li>
